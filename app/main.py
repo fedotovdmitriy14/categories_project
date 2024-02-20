26b5e318a44820838fbb8f242e27de3c69b42b8c
Федотов Дмитрий Вадимович <fedotovdv@mosreg.ru>
2024-02-21 01:46:43 +0300

    fixes

--- app/main.py
+++ app/main.py
@@ -32,23 +32,11 @@ async def shutdown():
 
 
 app.mount("/app/static", StaticFiles(directory="app/static"), name="static")
-
-
-@app.get("/front")
-async def get_index(request: Request):
-    return FileResponse("app/static/front.html")
-
+templates = Jinja2Templates(directory="app/templates")
 
 @app.get("/tree")
 async def get_index(request: Request):
     return FileResponse("app/static/tree.html")
-
-
-# @app.get("/single_category/{id}")
-# async def get_single_category(id: int, request: Request):
-#     return FileResponse("app/static/single_category.html")
-
-templates = Jinja2Templates(directory="app/templates")
 
 
 @app.get("/single_category/{id}", response_class=HTMLResponse)
@@ -56,19 +44,3 @@ async def read_item(request: Request, id: str):
     return templates.TemplateResponse(
         request=request, name="single_category.html", context={"id": id}
     )
-
-# @app.get("/single_category/{id}")
-# async def get_single_category(request: Request, id: int):
-#     return templates.TemplateResponse("single_category.html", {"request": request, "category_id": id})
-
-
-# @app.get("/single_category/{category_id}")
-# async def get_single_category(category_id: int, request: Request):
-#     # Logic to fetch data for the single category with category_id
-#     # This can include fetching data from your database or other sources
-#
-#     # Once you have the data, you can pass it to your HTML template
-#     # and render the template with the data
-#
-#     # Example: You can render a Jinja2 template passing the category data
-#     return templates.TemplateResponse("tree.html", {"category_id": category_id})
